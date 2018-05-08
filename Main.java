61dc904a0f0a28187455a8e858f853674e771fcb
TheNewExperience <39099271+TheNewExperience@users.noreply.github.com>
2018-05-08 20:21:05 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+
+    public static void main(String[] args)
+    {
+        Main c = new Main();
+
+    }
+
+    public boolean checkTriangleIsPosible(int a, int b, int c)
+    {
+
+        if()
+    }
+
+}
